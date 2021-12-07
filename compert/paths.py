642b591d89abe2ece1bdf26d229b00c238a4cc91
Leon Hetzel <leon.het@googlemail.com>
2021-12-07 06:27:58 +0100

    Added rdkit embedding (#46)
    Notebook is stored in `EMBEDDING_DIR/rdkit/`, paths are adjusted on embedding.py to include 200dim rdkit embeddings.

--- compert/paths.py
+++ compert/paths.py
@@ -4,4 +4,4 @@ ROOT = Path(__file__).parent.resolve().parent
 
 
 DATA_DIR = ROOT / "datasets"
-EMBEDDING_DIR = "/storage/groups/ml01/projects/2021_chemicalCPA_leon.hetzel/embeddings"
+EMBEDDING_DIR = Path("/storage/groups/ml01/projects/2021_chemicalCPA_leon.hetzel/embeddings")
