23f9a49d8732364ec9d69f05fbcd60802c279832
aszalata <aszalata@cellarity.com>
2025-01-12 14:06:23 +0000

    update paths

--- chemCPA/paths.py
+++ chemCPA/paths.py
@@ -2,9 +2,9 @@ from pathlib import Path
 
 ROOT = Path(__file__).parent.resolve().parent
 
-PROJECT_DIR = ROOT / "project_folder"
-DATA_DIR = PROJECT_DIR / "datasets"
-EMBEDDING_DIR = PROJECT_DIR / "embeddings"
+PROJECT_DIR = ROOT
+DATA_DIR = PROJECT_DIR / "data"
+EMBEDDING_DIR = PROJECT_DIR
 CHECKPOINT_DIR = PROJECT_DIR / "checkpoints"
 FIGURE_DIR = PROJECT_DIR / "figures"
 WB_DIR = PROJECT_DIR / "wandb"
